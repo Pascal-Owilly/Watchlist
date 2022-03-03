bf3766b2f3d7a477c5ad434a182a3b4916002604
Owilly <pasaclouma54@gmail.com>
2022-03-03 07:27:51 +0300

    form validation

--- app/views.py
+++ app/views.py
@@ -4,6 +4,12 @@ from .requests import get_movies
 from .requests import get_movies,get_movie
 from .requests import get_movies,get_movie,search_movie
 from flask import render_template,request,redirect,url_for
+from .forms import ReviewForm
+from .models import review
+
+
+Review = review.Review
+
 # Views
 @app.route('/')
 def index():
@@ -48,5 +54,17 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-   
-    
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)  
